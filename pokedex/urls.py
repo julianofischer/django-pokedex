a1917cc60071070bba5053b1e315a7ab366d6a97
Juliano Fischer Naves <julianofischer@gmail.com>
2022-05-31 11:42:21 -0300

    aula 31/05

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from pokemon.views import PokemonListView
+from pokemon.views import PokemonListView, TipoListView, PokemonDetailView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pokemons/', PokemonListView.as_view()),
+    path('pokemons/<int:pk>', PokemonDetailView.as_view()),
+    path('tipos/', TipoListView.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, 
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from pokemon.models import Pokemon, Tipo
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PokemonListView(ListView):
-    model = Pokemon
+    model = Pokemon 
+
+class PokemonDetailView(DetailView):
+    model = Pokemon 
+
+class TipoListView(ListView):
+    model = Tipo
